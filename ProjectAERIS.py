84bc27611c33afd1609672f3570aac49f5135f3a
Vimig <vimig.socrates@gmail.com>
2016-06-15 14:45:08 -0400

    added ProjectAERIS, fixed timex.py module

---
+++ ProjectAERIS.py
@@ -0,0 +1,18 @@
+import timex3-extractor from NLP_NLTK as timex2
+import sys
+
+
+def __init__():
+    
+
+def main():
+    filename = sys.argv[2]
+    rawTextFile = open(filename)
+    rawText = rawTextFile.read()
+
+    
+
+
+
+if __name__ == "__main__":
+    main()
